c66a266ed37d85ac4c666cb8a2c99ef1363a7904
Victoria Frantsuh <frantsuhvika@gmail.com>
2020-08-01 11:22:43 +0300

    added url database

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ var message = '';
 
 let pool;
 const URL = 'postgres://tjazoeceoycwvg:057769f8f752c8db628d229ea5c015893752d5a7fe45545740d473029bcc3b7e@ec2-54-147-209-121.compute-1.amazonaws.com:5432/d8s34dkaa8r8kh';
-  pool = new Pool ({connectionString:URL, ssl:false});
+  pool = new Pool ({connectionString:URL, ssl:true});
   // pool = new Pool({ user:'admin', host: 'localhost', database: 'school', password:'', port: 5432});
 
    //router(app)
@@ -44,10 +44,10 @@ const router = function(app) {
                             students.push(results.rows[i].fname+' '+results.rows[i].lname)
                         }
                         console.log('reslt******',students);
-                       // res.render('note',{students:students});
+                        res.render('note',{students:students});
                     }
                })
-                   res.render('note',{students:students});
+                  // res.render('note',{students:students});
                 }
             })
         }
