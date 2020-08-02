297c241caf3ef9caf13ec3cd805123048d932c91
Victoria Frantsuh <frantsuhvika@gmail.com>
2020-08-02 13:08:27 +0300

    added student res.redirect (/students)

--- routes/students.js
+++ routes/students.js
@@ -2,9 +2,9 @@ const {Pool} = require('pg');
 var message = '';
 
 let pool;
-const URL = 'postgres://tjazoeceoycwvg:057769f8f752c8db628d229ea5c015893752d5a7fe45545740d473029bcc3b7e@ec2-54-147-209-121.compute-1.amazonaws.com:5432/d8s34dkaa8r8kh';
-pool = new Pool ({connectionString:URL, ssl:false});
-//pool = new Pool({ user:'admin', host: 'localhost', database: 'school', password:'', port: 5432});
+//const URL = 'postgres://tjazoeceoycwvg:057769f8f752c8db628d229ea5c015893752d5a7fe45545740d473029bcc3b7e@ec2-54-147-209-121.compute-1.amazonaws.com:5432/d8s34dkaa8r8kh';
+//pool = new Pool ({connectionString:URL, ssl:false});
+pool = new Pool({ user:'admin', host: 'localhost', database: 'school', password:'', port: 5432});
 
 
 //router(app)
@@ -15,7 +15,7 @@ const router = function(app) {
  
     app.get('/students', (req,res) => {
          //dbh students local users
-        pool.query('Select fname,lname from users', (err,results)=>{
+        pool.query('Select fname,lname from students', (err,results)=>{
             if(err){
                 return err;
              } else {
@@ -32,12 +32,12 @@ const router = function(app) {
             return res.render('stud',{message:'Entered data is not correct'});
         } else{
             //dbh students local users
-            pool.query('INSERT INTO users (fname, lname, address, data_birth,sex) VALUES ($1,$2,$3,$4,$5)', 
+            pool.query('INSERT INTO students (fname, lname, address, data_birth,sex) VALUES ($1,$2,$3,$4,$5)', 
             [fname, lname, address, date_birth, sex], (error,results) =>{
                 if (error){
                   throw error;
                 } else {
-                    res.redirect('/stud');
+                    res.redirect('/students');
                 }
             })
         }
