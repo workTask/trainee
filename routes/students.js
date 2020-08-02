c41f1ed524897bb5d9df273f7637fb4d32fc8200
Victoria Frantsuh <frantsuhvika@gmail.com>
2020-08-02 13:18:33 +0300

    added student res.redirect (/students)

--- routes/students.js
+++ routes/students.js
@@ -37,7 +37,7 @@ const router = function(app) {
                 if (error){
                   throw error;
                 } else {
-                    res.redirect('/stud');
+                    res.redirect('/students');
                 }
             })
         }
